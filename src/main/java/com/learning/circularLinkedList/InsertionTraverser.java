6b55553ecd7adcf4e6f44abd769108f68804e71f
Shar2428 <111674774+Himanshu4256@users.noreply.github.com>
2025-07-28 00:26:06 +0530

    Circular Linked List Initial Commit

---
+++ src/main/java/com/learning/circularLinkedList/InsertionTraverser.java
@@ -0,0 +1,65 @@
+package com.learning.circularLinkedList;
+
+import java.util.Scanner;
+
+public class InsertionTraverser {
+
+    static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    Node head = null;
+    Node tail = null;
+
+    void creation(){
+        int data,n;
+
+        do{
+            System.out.println("Enter Data");
+            Scanner sc = new Scanner(System.in);
+            n = sc.nextInt();
+            Node newNode = new Node(n);
+
+            if (head == null){
+                head = newNode;
+                tail = newNode;
+                newNode.next = head;
+            }else {
+                newNode.next = head;
+                head = newNode;
+                tail.next = head;
+            }
+            System.out.println("Press 1 if you want to enter more data");
+            n = sc.nextInt();
+        }while (n==1);
+
+    }
+
+    void traverse(){
+        Node temp = head;
+        if (head == null){
+            System.out.println("CLL Not Exist");
+        }
+        else {
+            do {
+                System.out.println(temp.data);
+                temp = temp.next;
+            }
+            while (temp != head);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        InsertionTraverser insertionTraverser = new InsertionTraverser();
+                insertionTraverser.creation();
+                insertionTraverser.traverse();
+
+    }
+}
