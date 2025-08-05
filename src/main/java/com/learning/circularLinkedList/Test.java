6676a3ca649d9cfb115f1249e09a67db270fdc27
Himanshu4256 <111674774+Himanshu4256@users.noreply.github.com>
2025-08-06 00:56:38 +0530

    Test Commit

--- src/main/java/com/learning/circularLinkedList/Test.java
+++ src/main/java/com/learning/circularLinkedList/Test.java
@@ -1,7 +1,11 @@
 package com.learning.circularLinkedList;
 
+import java.util.logging.Logger;
+
 public class Test {
     public static void main(String[] args) {
+        final Logger logger = Logger.getLogger(Test.class.getName());
+        logger.info("Testing File");
         System.out.println("We Are Testing");
     }
 }
